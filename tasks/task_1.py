9f06b1d3ebe7ae4c45d21c18afe0f8cece36b805
nesuraisyn04 <nesuraisyn04@gmail.com>
2025-07-17 12:05:03 +0500

    refactor(task1): replaced args and kwargs with strings in fist print()

--- tasks/task_1.py
+++ tasks/task_1.py
@@ -4,7 +4,9 @@ from functools import wraps
 def log(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        print(f"Вызов: {func.__name__}{args or kwargs}", end="")
+        kwargs_arg = ", ".join([f"{key} = {val}" for key, val in kwargs.items()])
+        args_arg = ", ".join([f"{elem}" for elem in args])
+        print(f"Вызов: {func.__name__}({args_arg or kwargs_arg})", end="\n")
         result = func(*args, **kwargs)
         print(f"Результат: {result}")
         return result
