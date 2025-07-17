97f19dc32a2509283c9f57137213801626d85730
nesuraisyn04 <nesuraisyn04@gmail.com>
2025-07-17 13:05:13 +0500

    refactor(task2): added args kwargs checking method (check_args())

--- tasks/task_2.py
+++ tasks/task_2.py
@@ -1,12 +1,25 @@
 from functools import wraps
 
+NO_ARGS_KWARGS = 0
+NO_ARGS_KWARGS_STR = "No arguments"
+
+
+def check_args(args, kwargs):
+
+    if len(args) > NO_ARGS_KWARGS:
+        return str(args)
+    elif len(kwargs) > NO_ARGS_KWARGS:
+        return str(sorted(kwargs))
+
+    return NO_ARGS_KWARGS_STR
+
 
 def simple_cache(func):
     cache = {}
 
     @wraps(func)
     def wrapper(*args, **kwargs):
-        key_of_cache = (args or tuple(kwargs.items()))
+        key_of_cache = check_args(args, kwargs)
 
         if cache.get(key_of_cache) is None:
             cache[key_of_cache] = func(*args, **kwargs)
